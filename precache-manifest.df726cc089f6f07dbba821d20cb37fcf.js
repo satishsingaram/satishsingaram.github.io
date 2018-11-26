34f20b2f90eadcb6cf646eea749304d170a05bf6
Satish.Singaram <satish.singaram@bestbuy.com>
2018-11-26 19:20:52 +0530

    Updates

---
+++ precache-manifest.df726cc089f6f07dbba821d20cb37fcf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03dcd1f6f8d19b922297",
+    "url": "/static/js/main.03dcd1f6.chunk.js"
+  },
+  {
+    "revision": "9016f12fec1000e22dd7",
+    "url": "/static/js/1.9016f12f.chunk.js"
+  },
+  {
+    "revision": "9016f12fec1000e22dd7",
+    "url": "/static/css/1.9c79dd5a.chunk.css"
+  },
+  {
+    "revision": "5b97786d088ed61f5511fa7f095aab09",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d1768037a86d20948684e8917e477bd.js"
+  "/precache-manifest.df726cc089f6f07dbba821d20cb37fcf.js"
 );
 
 workbox.clientsClaim();
